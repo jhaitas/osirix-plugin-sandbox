7cf1320a0bbf059ab1af45d97c08adc441356cb8
John Haitas <john.haitas@vanderbilt.edu>
2010-06-08 14:33:03 -0500

    adding ConsoleSuccess plugin

---
+++ ConsoleSuccess/ConsoleSuccessFilter.h
@@ -0,0 +1,17 @@
+//
+//  ConsoleSuccessFilter.h
+//  ConsoleSuccess
+//
+//  Copyright (c) 2010 John. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "PluginFilter.h"
+
+@interface ConsoleSuccessFilter : PluginFilter {
+
+}
+
+- (long) filterImage:(NSString*) menuName;
+
+@end
---
+++ ConsoleSuccess/ConsoleSuccessFilter.m
@@ -0,0 +1,24 @@
+//
+//  ConsoleSuccessFilter.m
+//  ConsoleSuccess
+//
+//  Copyright (c) 2010 John. All rights reserved.
+//
+
+#import "ConsoleSuccessFilter.h"
+
+@implementation ConsoleSuccessFilter
+
+- (void) initPlugin
+{
+}
+
+- (long) filterImage:(NSString*) menuName
+{	
+	// In this plugin, we will simply print 'ConsoleSuccess Success!' to Console
+	NSLog(@"%@ Success!\n",menuName);
+	
+	return 0; // No Errors
+}
+
+@end
