dfa27a80925f91bd527d28eea77b36d903172456
John Haitas <john.haitas@vanderbilt.edu>
2010-09-29 16:43:08 -0500

    stupid little update to bash script to retrieve plugin generator

--- get-plugin-generator.bash
+++ get-plugin-generator.bash
@@ -3,7 +3,7 @@
 
 svn co https://osirixplugins.svn.sourceforge.net/svnroot/osirixplugins/_help
 cd _help
-unzip Osirix\ Plugin\ Generator.zip
-mv Osirix\ Plugin\ Generator.app ../
+unzip "Osirix Plugin Generator.zip"
+mv "Osirix Plugin Generator.app" ../
 cd ../
 rm -rf _help
