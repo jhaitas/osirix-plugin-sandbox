facd24dde1a4b0512a42ecb3f7f98940ebd5e410
John Haitas <john.haitas@vanderbilt.edu>
2011-01-04 15:57:42 -0600

    MPRHeaders does not need to import Cocoa

--- TenTwenty/OsiriX Headers/MPRHeaders.h
+++ TenTwenty/OsiriX Headers/MPRHeaders.h
@@ -5,9 +5,6 @@
 //  Created by John Haitas on 10/19/10.
 //  Copyright 2010 Vanderbilt University. All rights reserved.
 //
-
-#import <Cocoa/Cocoa.h>
-
 #import "MPRController.h"
 #import "MPRDCMView.h"
 #import "VRController.h"
